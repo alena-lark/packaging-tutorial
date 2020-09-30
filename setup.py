85ee40efe3bb155121d3bacc0501e8b29ff8b883
alena-lark <AllyLarck@yandex.com>
2020-09-30 17:11:41 +0300

    first commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="example-pkg-Tom", # Replace with your own username
+    version="0.0.1",
+    author="Tom Author",
+    author_email="larkina@scamt-itmo.ru",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
